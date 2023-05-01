293c51c18dfc994ff42666e64dd74269fdf6e9c0
Alphaalimamy <alphaalimamykamara@gmail.com>
2023-05-01 20:06:10 +0800

    CORS ORIGIN

--- codebattles/settings.py
+++ codebattles/settings.py
@@ -143,8 +143,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
-    'https://codebattle.up.railway.app',
-    'https://codebattles.up.railway.app/',
+    'https://*.up.railway.app/',
     'https://codebattles.dev'
 ]
 
