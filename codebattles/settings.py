9de95663e7501b6102d07bc029ae2b2eb7f877fc
Alphaalimamy <alphaalimamykamara@gmail.com>
2023-05-01 20:00:19 +0800

    CORS ORIGINS

--- codebattles/settings.py
+++ codebattles/settings.py
@@ -142,5 +142,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    'https://codebattle.up.railway.app',
+    'https://codebattles.up.railway.app/',
+    'https://codebattles.dev'
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
